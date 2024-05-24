cd91d0f90b4d626cc7a3c6dd5b16446d2bbcf84b
Sujay Goswami <sujaygoswami@Sujays-Mac-mini.local>
2024-05-24 18:48:25 +0530

    work update dated 24.05.2024 1st  push

--- html-master/JavaScript/init/main.js
+++ html-master/JavaScript/init/main.js
@@ -110,7 +110,7 @@ jQuery( window ).on( "orientationchange", function( event ) {
 
 
 // page banner slider
-jQuery('.page-banner-module .banner-slider').slick({
+jQuery('.page-banner-module .banner-slider:not(.no-slider)').slick({
   
 });
 
@@ -133,6 +133,41 @@ jQuery('.set-back').each(function(){
 
 });
 
+// product detail slider
+jQuery('.product-detail-slider').each(function(){
+var $status = jQuery(this).find('.pagingInfo');
+var $slickElement = jQuery(this).find('.slideshow');
+var prevbtn = jQuery(this).find('.slick-prev');
+var nextbtn = jQuery(this).find('.slick-next');
+
+
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  var i = (currentSlide ? currentSlide : 0) + 1;
+  $status.text(i + '/' + slick.slideCount);
+});
+
+$slickElement.slick({
+  autoplay: true,
+  dots: false,
+  arrows: true,
+  slidesToShow: 1,
+  infinite: false
+});
+
+var slicklist = jQuery(this).find('.slick-list');
+
+jQuery(this).find('.slick-arrow').wrapAll('<div class="custom-pagi"></div>');
+var custompagi = jQuery(this).find('.custom-pagi');
+jQuery(this).find('.pagingInfo').detach().appendTo(custompagi);
+jQuery(this).find('.slick-next').detach().insertAfter($status);
+
+jQuery(custompagi).detach().insertAfter(slicklist);
+
+jQuery(this).find('.download-expose').detach().appendTo(custompagi);
+
+    
+});
 
 
 
